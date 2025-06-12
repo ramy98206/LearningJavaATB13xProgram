7920e49728fa62f658ec18ec075f158a68711632
Ramesh Kata <ramesh.kata@gmail.com>
2025-06-12 19:42:08 +0530

    commit

--- src/ex_01_Java_Basic/Hello_Java.java
+++ src/ex_01_Java_Basic/Hello_Java.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basic;
 
 public class Hello_Java {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Java 123");
     }
 }
